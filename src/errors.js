d6d70cfa840b61c1d94aa80f0668bf9881b60550
mikostn <miko-git@stn.pl>
2019-02-14 15:03:04 +0100

    handle ralationships and errors

--- src/errors.js
+++ src/errors.js
@@ -19,4 +19,15 @@ export class HttpError extends Error {
     // this.body = body;
     this.name = 'HttpError';
   }
+  // constructor(message, status, body = null) {
+  //   console.log(status, 'message', message);
+  //   super(message);
+  //
+  //   console.log(status, 'message', message);
+  //
+  //   this.message = message.msg || JSON.stringify(message);
+  //   this.status = status;
+  //   this.body = body;
+  //   this.name = 'HttpError';
+  // }
 }
